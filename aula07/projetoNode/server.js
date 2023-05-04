b73ac9df818a60803db9314004c0c9c388efcdc9
Auto Logon <autologon@CSED.LAB>
2023-05-04 10:54:28 -0300

    aula de api

--- aula07/projetoNode/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-
-app.set('port', 7777); //escolha a porta que a aplicação vai rodar
-const server = app.listen(app.get('port'),()=>{
-      console.log("Servidor rodando na porta: " +server.address().port);
-})
